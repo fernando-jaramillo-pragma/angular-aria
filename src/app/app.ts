70f542dc8a994f5f3987476923908bc246a9827e
Fernando Jaramilllo <fernando.jaramillo@pragma.com.co>
2026-02-24 21:57:24 -0500

    feat(app): initialize app with accessibility setup prior to Angular Aria

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Tabs } from './tabs/tabs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrls: ['./app.css'],
+  template: `<app-tabs />`,
+  imports: [Tabs],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('angular-aria-pragma');
